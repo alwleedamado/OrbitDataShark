73f4a8fc6cb18698be6f8af7a0eebffbd76f34cd
Alwaleed Ahmed <alwleedamado@gmail.com>
2024-12-28 23:58:59 +0200

    Added Image url generator

---
+++ OrbitDataShark.DataGen/Generators/ImageUrl/ImageUrlGenerator.cs
@@ -0,0 +1,29 @@
+﻿using Bogus;
+using OrbitDataShark.DataGen.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrbitDataShark.DataGen.Generators.ImageUrl
+{
+    public class ImageUrlGenerator : Generator
+    {
+        public ImageUrlGenerator(GeneratorDescriptor descriptor) : base(descriptor)
+        {
+        }
+
+        public override Func<Faker, T, R> Generate<T, R>()
+        {
+            var descriptor = (ImageUrlGeneratorDescriptor)base.Descriptor;
+
+            return descriptor.Source switch
+            {
+                ImageUrlSource.Placeholder => (Faker f, T owner) => Convert<R>(f.Image.PlaceholderUrl(descriptor.Width, descriptor.Height)),
+                ImageUrlSource.PlaceImg => (Faker f, T owner) => Convert<R>(f.Image.PlaceImgUrl(descriptor.Width, descriptor.Height)),
+                _ => throw new NotImplementedException(),
+            };
+        }
+    }
+}
---
+++ OrbitDataShark.DataGen/Generators/ImageUrl/ImageUrlGeneratorDescriptor.cs
@@ -0,0 +1,21 @@
+﻿using OrbitDataShark.DataGen.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrbitDataShark.DataGen.Generators.ImageUrl
+{
+    public class ImageUrlGeneratorDescriptor : GeneratorDescriptor
+    {
+        public ImageUrlSource Source
+        {
+            get => (ImageUrlSource)_arguments["Source"];
+            set => _arguments["Source"] = value;
+        }
+        public required int Width { get; set; }
+        public required int Height { get; set; }
+        public override string GeneratorName => "ImageUrl";
+    }
+}
